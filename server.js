cf2eac204667a682463793df696a02857673f864
papunpanda26-stack <papunpanda26@gmail.com>
2026-07-17 15:07:53 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("✅ SafeWatch Live Server Running");
+});
+
+io.on("connection", (socket) => {
+
+    console.log("Device Connected :", socket.id);
+
+    socket.on("camera-frame", (frame) => {
+        socket.broadcast.emit("camera-frame", frame);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Device Disconnected :", socket.id);
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("SafeWatch Server Running On Port", PORT);
+});
